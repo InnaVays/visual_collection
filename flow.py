22715425c28ac6964ce9bd9256eef9ef3ff0458e
Inna Vays <129948497+InnaVays@users.noreply.github.com>
2022-02-13 19:36:00 +0100

    Create flow

---
+++ flow.py
@@ -0,0 +1,61 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import networkx as nx
+import numpy as np
+from matplotlib.sankey import Sankey
+import seaborn as sns
+
+
+def sankey_chart(flows, labels, title='Sankey Diagram'):
+
+    fig, ax = plt.subplots(figsize=(8, 6))
+    sankey = Sankey(ax=ax, unit=None)
+    for flow, label in zip(flows, labels):
+        sankey.add(flows=[flow], labels=[label])
+    sankey.finish()
+    ax.set_title(title)
+    plt.show()
+
+
+def waterfall_chart(categories, values, xlabel='Category', ylabel='Value', title='Waterfall Chart', bar_kwargs=None):
+
+    if bar_kwargs is None:
+        bar_kwargs = {}
+    
+    running_total = np.cumsum([0] + values[:-1])
+    fig, ax = plt.subplots(figsize=(8, 6))
+    colors = ['green' if v >= 0 else 'red' for v in values]
+    ax.bar(categories, values, bottom=running_total, color=colors, **bar_kwargs)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.set_title(title)
+    plt.show()
+
+
+def chord_diagram(matrix, labels, title='Chord Diagram'):
+
+    fig, ax = plt.subplots(figsize=(8, 8))
+    G = nx.DiGraph()
+    for i, label in enumerate(labels):
+        for j, value in enumerate(matrix[i]):
+            if value > 0:
+                G.add_edge(labels[i], labels[j], weight=value)
+    
+    pos = nx.circular_layout(G)
+    nx.draw(G, pos, with_labels=True, node_color='lightblue', edge_color='gray', arrows=True)
+    edge_labels = {(labels[i], labels[j]): matrix[i][j] for i in range(len(labels)) for j in range(len(labels)) if matrix[i][j] > 0}
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+    ax.set_title(title)
+    plt.show()
+
+
+def network_graph(edges, title='Network Graph'):
+
+    fig, ax = plt.subplots(figsize=(8, 8))
+    G = nx.Graph()
+    G.add_edges_from(edges)
+    pos = nx.spring_layout(G)
+    nx.draw(G, pos, with_labels=True, node_color='lightblue', edge_color='gray', ax=ax)
+    ax.set_title(title)
+    plt.show()
+
