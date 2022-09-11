2fdd8bcfd6e4e34131e9c59f6bd6721503537b50
Inna Vays <129948497+InnaVays@users.noreply.github.com>
2022-09-11 13:54:10 +0200

    Update change_in_time

--- change_in_time.py
+++ change_in_time.py
@@ -22,7 +22,12 @@ def line_chart(x, y, xlabel='Time', ylabel='Value', title='Line Chart', line_kwa
 
 
 def column_timeline(x, y, xlabel='Time', ylabel='Value', title='Column Timeline', bar_kwargs=None):
+    """
+    Creates a column chart to show changes over time.
+    
+    Best used for: Displaying a single series of change over time (e.g., revenue, population growth).
 
+    """
     if bar_kwargs is None:
         bar_kwargs = {}
     
@@ -35,7 +40,11 @@ def column_timeline(x, y, xlabel='Time', ylabel='Value', title='Column Timeline'
 
 
 def slope_chart(categories, values, xlabel='Category', ylabel='Value', title='Slope Chart', line_kwargs=None):
-
+    """ 
+    Creates a slope chart to show changes between 2-3 key points.
+    
+    Best used for: Showing simple changes between categories without missing key details.
+    """
     if line_kwargs is None:
         line_kwargs = {}
     
@@ -48,7 +57,11 @@ def slope_chart(categories, values, xlabel='Category', ylabel='Value', title='Sl
 
 
 def area_chart(x, y, xlabel='Time', ylabel='Value', title='Area Chart', area_kwargs=None):
-
+    """
+    Creates an area chart to show changes in total values over time.
+    
+    Best used for: Highlighting cumulative change but not ideal for seeing individual component changes.
+    """
     if area_kwargs is None:
         area_kwargs = {}
     
@@ -60,7 +73,11 @@ def area_chart(x, y, xlabel='Time', ylabel='Value', title='Area Chart', area_kwa
     plt.show()
 
 def stock_price_chart(dates, open_prices, close_prices, high_prices, low_prices, title='Stock Price Chart', candlestick_kwargs=None):
+    """
+    Creates a stock price chart showing open, close, high, and low values per time unit.
 
+    Best used for: Displaying daily stock movements with open/close and high/low points.
+    """
     if candlestick_kwargs is None:
         candlestick_kwargs = {}
 
@@ -76,7 +93,12 @@ def stock_price_chart(dates, open_prices, close_prices, high_prices, low_prices,
     plt.show()
 
 def fan_chart(x, y_mean, y_lower, y_upper, xlabel='Time', ylabel='Value', title='Fan Chart', line_kwargs=None, fill_kwargs=None):
+    """
+    Creates a fan chart to show uncertainty in future projections.
+    Best used for: Forecasting uncertainty, confidence intervals.
 
+
+    """
     if line_kwargs is None:
         line_kwargs = {}
     if fill_kwargs is None:
@@ -92,7 +114,13 @@ def fan_chart(x, y_mean, y_lower, y_upper, xlabel='Time', ylabel='Value', title=
     plt.show()
 
 def scatterplot_line_timeline(x, y, xlabel='Time', ylabel='Value', title='Connected Scatterplot Timeline', line_kwargs=None, scatter_kwargs=None):
- 
+    """
+    Creates a connected scatterplot timeline.
+
+    Best used for: Showing two-variable progression over time.
+
+    Parameters:
+    """
     if line_kwargs is None:
         line_kwargs = {}
     if scatter_kwargs is None:
@@ -107,7 +135,14 @@ def scatterplot_line_timeline(x, y, xlabel='Time', ylabel='Value', title='Connec
     plt.show()
 
 def calendar_heatmap(data, x_labels, y_labels, xlabel='Time', ylabel='Categories', title='Calendar Heatmap', heatmap_kwargs=None):
+    """
+    Creates a calendar heatmap to show temporal patterns.
 
+    Best used for: Showing daily, weekly, or monthly patterns.
+
+    Parameters:
+    - data: 2D array (matrix) of heatmap values.
+    """
     if heatmap_kwargs is None:
         heatmap_kwargs = {}
 
@@ -119,7 +154,11 @@ def calendar_heatmap(data, x_labels, y_labels, xlabel='Time', ylabel='Categories
     plt.show()
 
 def priestley_timeline(events, durations, xlabel='Time', ylabel='Events', title='Priestley Timeline', bar_kwargs=None):
+    """
+    Creates a Priestley timeline for date and duration visualization.
 
+    Best used for: Highlighting date and duration relationships.
+    """
     if bar_kwargs is None:
         bar_kwargs = {}
 
@@ -135,7 +174,13 @@ def priestley_timeline(events, durations, xlabel='Time', ylabel='Events', title=
     plt.show()
 
 def circles_timeline(x, y, sizes, xlabel='Time', ylabel='Categories', title='Circles Timeline', scatter_kwargs=None):
+    """
+    Creates a circles timeline to show discrete values over time.
 
+    Best used for: Representing discrete values of varying sizes across categories.
+
+    Parameters:
+    """
     if scatter_kwargs is None:
         scatter_kwargs = {}
 
@@ -147,7 +192,11 @@ def circles_timeline(x, y, sizes, xlabel='Time', ylabel='Categories', title='Cir
     plt.show()
 
 def seismogram(x, y, xlabel='Time', ylabel='Magnitude', title='Seismogram', line_kwargs=None):
+    """
+    Creates a seismogram-style chart for highly variable data.
 
+    Best used for: Displaying series with big variations.
+    """
     if line_kwargs is None:
         line_kwargs = {}
 
